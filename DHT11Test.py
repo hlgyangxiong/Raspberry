824cf99183bf4d214e070041cd4afd7646e85ba7
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-02-07 12:20:31 +0800

    Update and rename DHT11.py to DHT11Test.py

---
+++ DHT11Test.py
@@ -0,0 +1,80 @@
+import RPi.GPIO as GPIO  
+import time  
+  
+#定义IO口
+channel =4   
+data = []  
+j = 0  
+#设置IO编码模式  
+GPIO.setmode(GPIO.BCM)  
+  
+def GetData():
+    time.sleep(1)  
+  
+    GPIO.setup(channel, GPIO.OUT)  
+    GPIO.output(channel, GPIO.LOW)  
+    time.sleep(0.02)  
+    GPIO.output(channel, GPIO.HIGH)  
+    GPIO.setup(channel, GPIO.IN)  
+  
+    while GPIO.input(channel) == GPIO.LOW:  
+        continue  
+    while GPIO.input(channel) == GPIO.HIGH:  
+        continue  
+  
+    while j < 40:  
+        k = 0  
+        while GPIO.input(channel) == GPIO.LOW:  
+            continue  
+        while GPIO.input(channel) == GPIO.HIGH:  
+            k += 1  
+            if k > 100:  
+                break  
+            if k < 8:  
+                data.append(0)  
+            else:  
+                data.append(1)  
+  
+            j += 1  
+  
+    print "sensor is working."  
+    print data  
+def CheckData():  
+    humidity_bit = data[0:8]  
+    humidity_point_bit = data[8:16]  
+    temperature_bit = data[16:24]  
+    temperature_point_bit = data[24:32]  
+    check_bit = data[32:40]  
+  
+    humidity = 0  
+    humidity_point = 0  
+    temperature = 0  
+    temperature_point = 0  
+    check = 0  
+      
+    for i in range(8):  
+      humidity += humidity_bit[i] * 2 ** (7-i)  
+      humidity_point += humidity_point_bit[i] * 2 ** (7-i)  
+      temperature += temperature_bit[i] * 2 ** (7-i)  
+      temperature_point += temperature_point_bit[i] * 2 ** (7-i)  
+      check += check_bit[i] * 2 ** (7-i)  
+      
+    tmp = humidity + humidity_point + temperature + temperature_point  
+      
+    if check == tmp:  
+      print "temperature :", temperature, "*C, humidity :", humidity, "%"  
+    else:  
+      print "wrong"  
+      print "temperature :", temperature, "*C, humidity :", humidity, "% check :", check, ", tmp :", tmp  
+      
+    GPIO.cleanup()
+    
+def main():
+    while True:
+        GetData()
+        CheckData()
+        time.sleep(1000)
+        
+if __name__ == '__main__':
+    try:
+        main()
