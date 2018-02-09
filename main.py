f785b34d6bee8dab2b5598fd46ba771eb6e0f332
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-02-09 14:07:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ sensor_args = { '11': Adafruit_DHT.DHT11,
                 '2302': Adafruit_DHT.AM2302 }
 #if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
 sensor = sensor_args['11']
-pin = sys.argv['4']
+pin = sys.argv[2]
 #else:
 #    print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
 #    print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
