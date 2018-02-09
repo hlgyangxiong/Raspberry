5378a7cc7dc52935a34c20cdfccf12aaa0337932
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-02-09 14:20:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,27 +8,11 @@ sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
                 '2302': Adafruit_DHT.AM2302 }
 sensor = sensor_args['11']
-pin = 4
+DHT11pin = 4
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-#humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-# Un-comment the line below to convert the temperature to Fahrenheit.
-# temperature = temperature * 9/5.0 + 32
-
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-#if humidity is not None and temperature is not None:
-#    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-#else:
-#    print('Failed to get reading. Try again!')
-#    sys.exit(1)
 def GetTH():
     time.sleep(1)
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11pin)
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
     else:
@@ -36,5 +20,34 @@ def GetTH():
         sys.exit(1)
     time.sleep(1)
     
+    
+def init():  
+    GPIO.setwarnings(False)  
+    GPIO.setmode(GPIO.BOARD)  
+    GPIO.setup(12, GPIO.IN)  
+    GPIO.setup(11,GPIO.OUT)  
+    pass  
+    
+def beep():  
+        for i in range(1,6):  
+            GPIO.output(11, GPIO.LOW) 
+            time.sleep(0.5)  
+            GPIO.output(11, GPIO.HIGH)  
+            time.sleep(0.5)  
+            print "the Buzzer will make sound"  
+ 
+def detct():  
+    for i in range(1, 31):  
+        if GPIO.input(12) == True:  
+            print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+"  Someone is closing!"  
+            beep()  
+        else:  
+            GPIO.output(11, GPIO.HIGH)  
+            print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+"  Noanybody!"  
+        time.sleep(6) 
+        
+init()         
+        
 while True:
     GetTH()
+    detct()
