30e180b2f66b70069858cee2f23c0f16366ec36a
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-03-19 15:36:13 +0800

    Update led.c

--- led.c
+++ led.c
@@ -1,12 +1,40 @@
-#include <wiringPi.h>  
+#include <wiringPi.h> 
 
-int main(void)  
-{  
-  wiringPiSetup() ;  
-  pinMode (0, OUTPUT) ;  
-  for(;;)   
+#define LEDG1	23
+#define LEDY1	24
+#define LEDR1	25	
+
+#define LEDG2	27
+#define LEDY2	28
+#define LEDR2	29	
+
+void init(void)
+{
+    wiringPiSetup() ; 
+    pinMode(LEDG1,OUTPT);
+    pinMode(LEDY1,OUTPT);
+    pinMode(LEDR1,OUTPT);
+    pinMode(LEDG2,OUTPT);
+    pinMode(LEDY2,OUTPT);
+    pinMode(LEDR2,OUTPT);
+}
+
+int main(void)
+{
+   for(;;)   
   {  
-    digitalWrite(0, HIGH) ; delay (500) ;  
-    digitalWrite(0,  LOW) ; delay (500) ;  
-  }  
-}  
+    digitalWrite(LEDG1, HIGH) ;
+    digitalWrite(LEDY1, HIGH) ;
+    digitalWrite(LEDR1, HIGH) ;
+    digitalWrite(LEDG2, HIGH) ;
+    digitalWrite(LEDY2, HIGH) ;
+    digitalWrite(LEDR2, HIGH) ;
+    delay (500) ;  
+    digitalWrite(LEDG1,  LOW) ;
+    digitalWrite(LEDY1,  LOW) ;
+    digitalWrite(LEDR1,  LOW) ; 
+    digitalWrite(LEDG2,  LOW) ;
+    digitalWrite(LEDY2,  LOW) ;
+    digitalWrite(LEDR2,  LOW) ;delay (500) ;  
+  } 
+}
