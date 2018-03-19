a0090394b8318705863dbcdc5bf8e068be0dbaff
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-03-19 15:55:09 +0800

    Update led.c

--- led.c
+++ led.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
-#include <wiringPi.h>
+#include <wiringPi.h> 
 
-// LED Pin - wiringPi pin 0 is BCM_GPIO 17.
+#define LEDG1	23
+#define LEDY1	24
+#define LEDR1	25	
 
-#define LED1     0
-#define LED2     23
+#define LEDG2	27
+#define LEDY2	28
+#define LEDR2	29	
 
-int main (void)
+void init(void)
 {
-  printf ("Raspberry Pi - Gertboard Blink\n") ;
+    wiringPiSetup() ; 
+    pinMode(LEDG1,OUTPUT);
+    pinMode(LEDY1,OUTPUT);
+    pinMode(LEDR1,OUTPUT);
+    pinMode(LEDG2,OUTPUT);
+    pinMode(LEDY2,OUTPUT);
+    pinMode(LEDR2,OUTPUT);
+}
 
-  wiringPiSetup () ;
-
-  pinMode (LED1, OUTPUT) ;
-  pinMode (LED2, OUTPUT) ;
-
-  for (;;)
-  {
-    digitalWrite (LED1, 1) ;     // On
-    digitalWrite (LED2, 1) ;
-    delay (500) ;               // mS
-    digitalWrite (LED1, 0) ;     // Off
-    digitalWrite (LED2, 0) ;
-    delay (500) ;
-  }
-  return 0 ;
+int main(void)
+{
+    init();
+   for(;;)   
+  {  
+    digitalWrite(LEDG1, HIGH) ;
+    digitalWrite(LEDY1, HIGH) ;
+    digitalWrite(LEDR1, HIGH) ;
+    digitalWrite(LEDG2, HIGH) ;
+    digitalWrite(LEDY2, HIGH) ;
+    digitalWrite(LEDR2, HIGH) ;
+    delay (500) ;  
+    digitalWrite(LEDG1,  LOW) ;
+    digitalWrite(LEDY1,  LOW) ;
+    digitalWrite(LEDR1,  LOW) ; 
+    digitalWrite(LEDG2,  LOW) ;
+    digitalWrite(LEDY2,  LOW) ;
+    digitalWrite(LEDR2,  LOW) ;
+    delay (500) ;  
+  } 
 }
