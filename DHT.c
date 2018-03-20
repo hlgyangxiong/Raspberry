bf3f86e8d293d951756253548faf973bfd0cf8f7
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-03-20 14:04:39 +0800

    Update DHT.c

--- DHT.c
+++ DHT.c
@@ -1,78 +1,143 @@
+/*
+vim:et:sta:sts=4:sw=4:ts=8:tw=79:
+ *
+ *
+ * This reads humidity and temperature data from a DHT11 sensor, connected to a
+ * Raspberry Pi GPIO pin 7.
+ *
+ * It requires the WiringPi library and is based on the code found in:
+ * http://www.rpiblog.com/2012/11/interfacing-temperature-and-humidity.html
+ *
+ *
+*/
+
 #include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <time.h>
 #define MAX_TIME 85
-#define GpioPort 7
- 
-//存储读取的数据
-int dht11_val[5] = { 0, 0, 0, 0, 0 };
-//此函数读取温度值
-int dht11_read_val(){
-uint8_t lststate = HIGH;//HIGH在wiringPi.h定义为1
-uint8_t counter = 0;//计数器
-uint8_t j = 0, i;
-//初始化数组为0
-for (i = 0; i < 5; i++)
-dht11_val[i] = 0;
-//发送开始信号
-pinMode(GpioPort, OUTPUT);//设置GPIO为输出
-digitalWrite(GpioPort, LOW);//保持低电平至少18ms
-delay(18);
-digitalWrite(GpioPort, HIGH);//保持高电平20-40us
-delayMicroseconds(40);
-//开始接受dht响应信号
-pinMode(GpioPort, INPUT);//设置GPIO为输入
-//开始等待温度传感器回应（根据数据手册温度传感器回应时会拉低数据线）
-for (i = 0; i < MAX_TIME; i++)
-{
-counter = 0;
-while (digitalRead(GpioPort) == lststate){//如果此时的电平和当前电平相等说明电平没有发生变化，所以继续等下一次循环
-counter++;
-delayMicroseconds(1);
-if (counter == 255)//如果在循环了255+1次之后温度传感器还未变换数据线则退出此次循环
-break;
+#define MAX_TRIES 100
+#define DHT11PIN 7
+
+int dht11_val[5]={0,0,0,0,0};
+
+int dht11_read_val(int *h, int *t) {
+    uint8_t lststate=HIGH;
+    uint8_t counter=0;
+    uint8_t j=0,i;
+    
+    for (i=0;i<5;i++) {
+         dht11_val[i]=0;
+    }
+
+    pinMode(DHT11PIN,OUTPUT);
+    digitalWrite(DHT11PIN,LOW);
+    delay(18);
+    digitalWrite(DHT11PIN,HIGH);
+    delayMicroseconds(40);
+    pinMode(DHT11PIN,INPUT);
+
+    for (i=0;i<MAX_TIME;i++) {
+        counter=0;
+        while (digitalRead(DHT11PIN)==lststate){
+            counter++;
+            delayMicroseconds(1);
+            if (counter==255)
+                break;
+        }
+        lststate=digitalRead(DHT11PIN);
+        if (counter==255)
+             break;
+        // top 3 transitions are ignored
+        if ((i>=4) && (i%2==0)) {
+            dht11_val[j/8]<<=1;
+            if(counter>16)
+                dht11_val[j/8]|=1;
+            j++;
+        }
+    }
+
+    // verify cheksum and print the verified data
+    if ((j>=40) && (dht11_val[4]==((dht11_val[0]+dht11_val[1]+dht11_val[2]+dht11_val[3])& 0xFF))) {
+        // Only return the integer part of humidity and temperature. The sensor
+        // is not accurate enough for decimals anyway 
+        *h = dht11_val[0];
+        *t = dht11_val[2];
+        return 0;
+    }
+    else {
+        // invalid data
+        return 1;
+    }
 }
-lststate = digitalRead(GpioPort);//读取数据线最新的状态值
-if (counter == 255)//如果在循环了255+1次之后温度传感器还未变换数据线则退出此次循环
-break;
-//前三次被忽略可能是在等温度传感器响应（这玩意不是数据手册上说的反应那么快的....）
-if ((i >= 4) && (i % 2 == 0)){//对2取余是因为偶数电平是用来传输数据的（从数据手册可以看出）
-dht11_val[j / 8] <<= 1;//左移一位（此时最后一位为0）
-if (counter > 16) //此处时间很长代表此处为1
-dht11_val[j / 8] |= 1; //将数据1写入
-j++;
-}
-}
-/*
-开始处理数据
- 
-1.因为一次完整的数据输出为40位，所以j应大于40
-2.dht11_val[4]为校验和，此处判断校验和是否正确
-3.只打印dht11_val[0], dht11_val[2]是因为dht11_val[1], dht11_val[3]是小数部分，根据数据手册小数始终为零
-*/
-if ((j >= 40) && (dht11_val[4] == ((dht11_val[0] + dht11_val[1] + dht11_val[2] + dht11_val[3]) & 0xFF))){
-printf("RH:%d,TEMP:%d\n", dht11_val[0], dht11_val[2]);
-return 1;
-}
-else
-return 0;
-}
-int main(void){
-//重复5次进行尝试
-int attempts = 5;
-//初始化GPIO
-if (wiringPiSetup() == -1)
-exit(1);
-//进行5次尝试
-while (attempts){
-//获取温度值
-int success = dht11_read_val();
-//获取成功后退出
-if (success)
-break;
-attempts--;
-delay(2500);
-}
-return 0;
+
+int main(int argc, char *argv[]) {
+    int h; //humidity
+    int t; //temperature in degrees Celsius
+    float f; //temperature in degrees Fahrenheit
+    int fahrenheit = 0; //use degrees Celsius by default
+
+    int print_time = 0;
+    time_t current_time = time(NULL);
+    struct tm tm = *localtime(&current_time);
+
+    int opt;
+    while ((opt = getopt(argc, argv, "dfh")) != -1) {
+        switch (opt) {
+            case 'd': print_time = 1; break;
+            case 'f': fahrenheit = 1; break;
+            default:
+                printf("Usage: %s [-dfh]\n\n", argv[0]);
+                printf("OPTIONS:\n");
+                printf("    -d,    Print date and time before values\n");
+                printf("    -f,    Print temperature using the Fahrenheit scale\n");
+                printf("    -h,    This help message\n");
+                exit(1);
+        }
+
+    }
+
+    // error out if wiringPi can't be used
+    if (wiringPiSetup()==-1) {
+        printf("Error interfacing with WiringPi\n");
+        exit(1);
+    }
+    
+    // throw away the first 3 measurements
+    for (int i=0; i<3; i++) {
+        dht11_read_val(&h, &t);
+        delay(3000);
+    }
+
+    // read the sensor until we get a pair of valid measurements
+    // but bail out if we tried too many times
+    int retval=1;
+    int tries=0;
+    while (retval != 0 && tries < MAX_TRIES)
+    {
+        retval = dht11_read_val(&h, &t);
+        if (retval == 0) {
+            if (print_time == 1) {
+                printf("%4d-%02d-%02d,", tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday);
+                printf("%02d:%02d,", tm.tm_hour, tm.tm_min);
+            }
+            printf("%d,", h);
+            if (fahrenheit == 1) {
+                f = t*9./5.+32;
+                printf("%.1f\n", f);
+            }
+            else
+                printf("%d\n", t);
+        } else {
+            delay(3000);
+    }
+        tries += 1;
+    }
+    if (tries < MAX_TRIES)
+        return 0;
+    else
+        return 1;
+
 }
