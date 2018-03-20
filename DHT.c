e5bf139bef011d69ac0cdd0d2a75a15878d6166f
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-03-20 14:20:38 +0800

    Update DHT.c

--- DHT.c
+++ DHT.c
@@ -65,10 +65,13 @@ int dht11_read_val(int *h, int *t) {
         // is not accurate enough for decimals anyway 
         *h = dht11_val[0];
         *t = dht11_val[2];
+        printf( "Humidity = %d.%d %% Temperature = %d.%d *C (%.1f *F)\n",  
+            dht11_val[0], dht11_val[1], dht11_val[2], dht11_val[3], f ); 
         return 0;
     }
     else {
         // invalid data
+        printf( "Data not good, skip\n" );
         return 1;
     }
 }
@@ -77,67 +80,18 @@ int main(int argc, char *argv[]) {
     int h; //humidity
     int t; //temperature in degrees Celsius
     float f; //temperature in degrees Fahrenheit
-    int fahrenheit = 0; //use degrees Celsius by default
-
-    int print_time = 0;
-    time_t current_time = time(NULL);
-    struct tm tm = *localtime(&current_time);
-
-    int opt;
-    while ((opt = getopt(argc, argv, "dfh")) != -1) {
-        switch (opt) {
-            case 'd': print_time = 1; break;
-            case 'f': fahrenheit = 1; break;
-            default:
-                printf("Usage: %s [-dfh]\n\n", argv[0]);
-                printf("OPTIONS:\n");
-                printf("    -d,    Print date and time before values\n");
-                printf("    -f,    Print temperature using the Fahrenheit scale\n");
-                printf("    -h,    This help message\n");
-                exit(1);
-        }
-
-    }
-
+  
+    
     // error out if wiringPi can't be used
     if (wiringPiSetup()==-1) {
         printf("Error interfacing with WiringPi\n");
         exit(1);
     }
     
-    // throw away the first 3 measurements
-    for (int i=0; i<3; i++) {
-        dht11_read_val(&h, &t);
-        delay(3000);
-    }
-
-    // read the sensor until we get a pair of valid measurements
-    // but bail out if we tried too many times
-    int retval=1;
-    int tries=0;
-    while (retval != 0 && tries < MAX_TRIES)
-    {
-        retval = dht11_read_val(&h, &t);
-        if (retval == 0) {
-            if (print_time == 1) {
-                printf("%4d-%02d-%02d,", tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday);
-                printf("%02d:%02d,", tm.tm_hour, tm.tm_min);
-            }
-            printf("%d,", h);
-            if (fahrenheit == 1) {
-                f = t*9./5.+32;
-                printf("%.1f\n", f);
-            }
-            else
-                printf("%d\n", t);
-        } else {
-            delay(3000);
-    }
-        tries += 1;
-    }
-    if (tries < MAX_TRIES)
-        return 0;
-    else
-        return 1;
+    while ( 1 )  
+    {  
+        dht11_read_val(&h, &t);  
+        delay( 1000 ); /* wait 1sec to refresh */  
+    }  
 
 }
