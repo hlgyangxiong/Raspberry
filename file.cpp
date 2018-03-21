fdc95b61024e258d70a006a012a5f5290f1c9913
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-03-21 18:34:50 +0800

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h> 
 #include <stdlib.h> 
-int main(int argc, char *argv[]) 
+int main() 
 { 
     FILE *fp; 
-    if((fp = fopen("f:\\ok.txt","a++")) == NULL)
+    if((fp = fopen("data.txt","a+")) == NULL)
     { 
 
     } 
-    if((fp=fopen("f:\\ok.txt","w+"))==NULL)//如果文件不存在，新建文件，写数据 
+    if((fp=fopen("data.txt","w+"))==NULL)//如果文件不存在，新建文件，写数据 
     { 
         printf("Can not open file.\n"); 
         exit(0); 
     } 
-    fputs("\n123456789000\n",fp);//试验数据，文件存在时尝试，文件不存在时尝试 
+    fputs("123456789000\n",fp);//试验数据，文件存在时尝试，文件不存在时尝试 
     fclose(fp); 
     return 0; 
 }
