fed98aa35601b0d6b5cbff5262e0c2b7157c7148
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-03-21 18:25:26 +0800

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -1,8 +1,18 @@
-#include <fstream.h>
- 
-void main()
-{
-    ofstream SaveFile(“cpp-home.txt”);
-    SaveFile << “Hello World, from www.cpp-home.com and Loobian!”;
-    SaveFile.close();
+#include <stdio.h> 
+#include <stdlib.h> 
+int main(int argc, char *argv[]) 
+{ 
+    FILE *fp; 
+    if((fp = fopen("f:\\ok.txt","a++")) == NULL)
+    { 
+
+    } 
+    if((fp=fopen("f:\\ok.txt","w+"))==NULL)//如果文件不存在，新建文件，写数据 
+    { 
+        printf("Can not open file.\n"); 
+        exit(0); 
+    } 
+    fputs("\n123456789000\n",fp);//试验数据，文件存在时尝试，文件不存在时尝试 
+    fclose(fp); 
+    return 0; 
 }
