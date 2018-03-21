73bff75cb5cdba18cc49fbb86b57615f93eb28b3
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-03-21 20:35:26 +0800

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -2,17 +2,15 @@
 #include <stdlib.h> 
 int main() 
 { 
+    int i = 1234;
     FILE *fp; 
-    if((fp = fopen("data.txt","a+")) == NULL)
-    { 
-
-    } 
+    
     if((fp=fopen("data.txt","w+"))==NULL)//如果文件不存在，新建文件，写数据 
     { 
         printf("Can not open file.\n"); 
         exit(0); 
     } 
-    fputs("123456789000\n",fp);//试验数据，文件存在时尝试，文件不存在时尝试 
+    fwrite(itoa(i),fp);//试验数据，文件存在时尝试，文件不存在时尝试 
     fclose(fp); 
     return 0; 
 }
