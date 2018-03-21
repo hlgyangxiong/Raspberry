ee1d730a9fd4e8c3cdde146ea835485a7d1588f1
hlgyangxiong <hlgyangxiong@foxmail.com>
2018-03-21 16:52:26 +0800

    Update main.c

--- main.c
+++ main.c
@@ -19,11 +19,12 @@
 
 int dht11_val[5]={0,0,0,0,0}; 
 
+
 void init(void);
 void LED(int number);
 void Fan(int number);
 void Bell();
-int dht11_read_val(int &Humidity,int &Temperature) 
+int dht11_read_val(int *Humidity,int *Temperature);
 
 
 
@@ -39,7 +40,7 @@ int main(void)
 
     while(1)
     {
-      if(dht11_read_val(Humidity,Temperature))
+      if(dht11_read_val(&Humidity,&Temperature))
       {
         printf("Humidity = %d.%d %% Temperature = %d.%d *C \n",Humidity,dht11_val[1],Temperature,dht11_val[3]);
       }   
@@ -49,7 +50,7 @@ int main(void)
       delay(1000);
       for(i = 0; i < 4; i++)
       {
-        if(dht11_read_val(Humidity,Temperature) == 1)
+        if(dht11_read_val(&Humidity,&Temperature) == 1)
         {
           printf("Humidity = %d.%d %% Temperature = %d.%d *C \n",dht11_val[0],dht11_val[1],dht11_val[2],dht11_val[3]);
           break;
@@ -198,7 +199,7 @@ void Bell()
     }
 }
 
-int dht11_read_val(int &Humidity,int &Temperature)  
+int dht11_read_val(int *Humidity,int *Temperature)  
 {  
   int counter=0;  
   uint8_t i;   
@@ -242,8 +243,8 @@ int dht11_read_val(int &Humidity,int &Temperature)
   // verify cheksum and print the verified data 
   if(dht11_val[4]==((dht11_val[0]+dht11_val[1]+dht11_val[2]+dht11_val[3])& 0xFF)) 
   {  
-    Humidity    = dht11_val[0];
-    Temperature = dht11_val[2];
+    *Humidity    = dht11_val[0];
+    *Temperature = dht11_val[2];
     return 1;
   }  
   else  
